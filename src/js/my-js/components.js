e108eda0b98ea8a50927a9671ec3eda9695ff46d
tz7gg <69346690+tz7gg@users.noreply.github.com>
2022-11-02 21:02:45 +0500

    upgrade :)

---
+++ src/js/my-js/components.js
@@ -0,0 +1,40 @@
+function createFilterItem(titleText, bodyText, id) {
+    const item = document.createElement("div")
+    const checkbox = document.createElement("input")
+    const wrap = document.createElement("div")
+    const title = document.createElement("div")
+    const text = document.createElement("div")
+
+    item.classList.add('filter__items__item')
+    checkbox.classList.add('filter__items__item__checkbox')
+    wrap.classList.add('filter__items__item__wrap')
+    title.classList.add('filter__items__item__wrap__title')
+    text.classList.add('filter__items__item__wrap__text')
+
+    item.dataset.id = id
+
+    checkbox.type = 'checkbox'
+
+    title.title = titleText
+    title.innerHTML = titleText
+
+    text.innerHTML = bodyText
+
+    item.appendChild(checkbox)
+    item.appendChild(wrap)
+    wrap.appendChild(title)
+    wrap.appendChild(text)
+
+    checkbox.onclick = () => {
+        if (checkbox.checked) {
+            localStorage.setItem(id, true);
+        } else {
+            localStorage.setItem(id, false);
+        }
+    }
+    if (localStorage.getItem(id) === 'true') {
+        checkbox.checked = true
+    }
+
+    return item
+}
--- src/js/my-js/filter.js
+++ src/js/my-js/filter.js
@@ -1,55 +1,60 @@
 window.addEventListener('load', () => {
 
-    let elements
-    let searchBtn = document.querySelector('.filter__search__btn')
-    let searchInput = document.querySelector('.filter__search__input')
+    const elements = []
+    const searchBtn = document.querySelector('.filter__search__btn')
+    const searchInput = document.querySelector('.filter__search__input')
+    const queryLink = 'https://jsonplaceholder.typicode.com/posts/?_start=0&_limit=7'
 
     searchInput.value = window.location.search.replace('?search=', '').replace('%20', ' ')
 
-    fetch('https://jsonplaceholder.typicode.com/posts/?_start=0&_limit=7')
-        .then((res) => res.json())
-        .then(
-            (result) => {
-                elements = result
-                renderEls(searchInput.value, result)
-            },
-            (error) => {
-                console.log(error);
-            }
-        );
+    API(queryLink)
 
-    function renderEls(searchText, elements) {
-        let content = ''
+    searchInput.addEventListener('keydown', function(e) {
+        if (e.keyCode === 13) {
+            setLinkAndRerender()
+        }
+    })
+
+    searchBtn.onclick = () => {
+        setLinkAndRerender()
+    }
+
+    function API(link) {
+        fetch(link)
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    result.forEach(el => {
+                        elements.push(el)
+                    })
+                    renderItems(searchInput.value, result)
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+        return false
+    }
+
+    function renderItems(searchText, elements) {
         const wrap = document.querySelector('.filter__items')
-
+        wrap.innerHTML = ''
         elements.forEach(element => {
             if (element.title.includes(searchText)) {
-                content += `
-					<div class="filter__items__item">
-						<input type="checkbox" class="filter__items__item__checkbox">
-						<div class="filter__items__item__wrap">
-							<div class="filter__items__item__wrap__title" title="${element.title}">
-							${element.title}
-							</div>
-							<div class="filter__items__item__wrap__text">
-							${element.body}
-						</div>
-                    </div>
-                </div>
-			`
+                wrap.appendChild(createFilterItem(element.title, element.body, element.id))
             }
 
         });
-        wrap.innerHTML = content
         return false
     }
-    searchBtn.onclick = () => {
-        renderEls(searchInput.value, elements)
+
+    function setLinkAndRerender() {
+        renderItems(searchInput.value, elements)
         if (searchInput.value) {
             window.history.pushState(null, null, `?search=${searchInput.value}`);
         } else {
             window.history.pushState(null, null, '/');
         }
-
+        return false
     }
 })
